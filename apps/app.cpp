98959a7edbfb3ecb68e48c0cc57747620cf0993c
LT2H <letranhuuhung@gmail.com>
2025-04-02 17:08:57 +0700

    feat: implement non-blocking HTTP GET request with async response handling

--- apps/app.cpp
+++ apps/app.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 #ifdef _WIN32
 #define _WIN32_WINNT 0x0A00
@@ -11,16 +12,50 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+// std::vector<char> buffer(20 * 1024); // 20kb buffer
+std::vector<char> buffer(1 * 1024); // 1kb buffer
+
+void grab_some_data(asio::ip::tcp::socket& socket)
+{
+    // The instruction we primed the context with was this read_some func, so it'll
+    // read as much as it can and display it to the console
+    socket.async_read_some(asio::buffer(buffer.data(), buffer.size()),
+                           [&](std::error_code ec, std::size_t length)
+                           {
+                               if (!ec)
+                               {
+                                   std::cout << "\n\nRead " << length
+                                             << " bytes\n\n";
+
+                                   for (const auto& c : buffer)
+                                   {
+                                       std::cout << c;
+                                   }
+
+                                   grab_some_data(socket);
+                               }
+                           });
+}
+
 int main()
 {
     asio::error_code ec;
 
-    // Create a "context" - essentially the platform specific interface
+    // Create a "context" - essentially the platform specific interface - which is
+    // the space where asio can do its work
     asio::io_context context;
 
+    // Give some fake tasks to asio so the context doesn't finish
+    asio::io_context::work idle_work{ context };
+
+    // Start the context
+    // Allow that context to run in its own thread, so if it does need to stop and
+    // wait, it doesn't block the main program execution
+    std::thread thr_context{ std::thread([&] { context.run(); }) };
+
     // Get the address of somewhere we wish to connect to
-    // asio::ip::tcp::endpoint endpoint{ asio::ip::make_address("142.250.190.78​", ec), 443 };
-    asio::ip::tcp::endpoint endpoint{ asio::ip::make_address("51.38.81.49", ec), 80 };
+    asio::ip::tcp::endpoint endpoint{ asio::ip::make_address("51.38.81.49", ec),
+                                      80 };
 
     // Create a socket, the context will deliver the impl
     asio::ip::tcp::socket socket{ context };
@@ -39,30 +74,21 @@ int main()
 
     if (socket.is_open())
     {
-        std::string s_request
-        {
-            "GET / HTTP/1.1\r\n"
-            "Host: example.com\r\n"
-            "Connection: close\r\n\r\n"
-        };
+        // We prime the asio context with an instruction to read some data if it's
+        // available
+        grab_some_data(socket);
 
-        socket.write_some(asio::buffer(s_request.data(), s_request.size()), ec);
+        // Once it's primed, we then write our HTTP request, and then we wait
+        std::string request{ "GET / HTTP/1.1\r\n"
+                             "Host: example.com\r\n"
+                             "Connection: close\r\n\r\n" };
 
-        socket.wait(socket.wait_read);
+        socket.write_some(asio::buffer(request.data(), request.size()), ec);
 
-        size_t bytes{ socket.available() };
-        std::cout << "Bytes Available: " << bytes << std::endl;
-        
-        if (bytes > 0)
-        {
-            std::vector<char> v_buffer(bytes);
-            socket.read_some(asio::buffer(v_buffer.data(), v_buffer.size()), ec);
-            
-            for (auto c : v_buffer)
-            {
-                std::cout << c;
-            }
-        }
+        // Program does something else, while asio handles data transfer in
+        // background
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(2000ms);
     }
 
     std::cin.get();
