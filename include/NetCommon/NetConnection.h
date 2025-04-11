df0b28d0051226f491c4cbcfd9456c7ba0c84a52
LT2H <letranhuuhung@gmail.com>
2025-04-11 14:28:13 +0700

    feat: implement mechanism to keep client connections alive

--- include/NetCommon/NetConnection.h
+++ include/NetCommon/NetConnection.h
@@ -4,6 +4,8 @@
 #include "NetTsQueue.h"
 #include "NetMessage.h"
 #include "NetCommon.h"
+#include "asio/io_context.hpp"
+#include <cstdint>
 
 namespace fw
 {
@@ -13,14 +15,40 @@ template <typename T>
 class Connection : public std::enable_shared_from_this<Connection<T>>
 {
   public:
-    Connection() {}
+    enum class Owner
+    {
+        server,
+        client,
+    };
+
+    Connection(Owner parent, asio::io_context& asio_context,
+               asio::ip::tcp::socket socket, TsQueue<OwnedMessage<T>>& message_in)
+        : asio_context_{ asio_context }, socket_{ std::move(socket) },
+          messages_in_{ message_in }
+    {
+        owner_type_ = parent;
+    }
 
     virtual ~Connection() {}
 
+    uint32_t id() const { return id_; }
+
   public:
+    // For server only
+    void connect_to_client(uint32_t id = 0)
+    {
+        if (owner_type_ == Owner::server)
+        {
+            if (socket_.is_open())
+            {
+                id_ = id;
+            }
+        }
+    }
+
     bool connect_to_server();
     bool disconnect();
-    bool is_connected() const;
+    bool is_connected() const { return socket_.is_open();  };
 
   public:
     bool send(const Message<T>& msg);
@@ -39,6 +67,10 @@ class Connection : public std::enable_shared_from_this<Connection<T>>
     // this connection. Note it is a referece as the "owner" of this connection is
     // expected to provide a queue
     TsQueue<OwnedMessage<T>>& messages_in_;
+
+    // The "owner" decides how some of the connection behaves
+    Owner owner_type_{ Owner::server };
+    uint32_t id_{ 0 };
 };
 } // namespace net
 
