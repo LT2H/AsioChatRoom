673593bf8c09fac069623ce68bdf3467f4c36359
LT2H <letranhuuhung@gmail.com>
2025-05-06 13:29:34 +0700

    refactor: add ClientInfo class s for handling more detailed info

---
+++ apps/Server/ClientInfo.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <NetCommon/FwNet.h>
+#include "CustomMsgTypes.h"
+
+struct ClientInfo
+{
+    std::shared_ptr<fw::net::Connection<CustomMsgTypes>> conn;
+    std::array<char, fw::net::array_size> name{ "Anon" };
+
+    std::array<float, 3> color{ 1.0f, 1.0f, 1.0f };
+
+    constexpr std::string to_string() const { return std::string{ name.data() }; }
+};
