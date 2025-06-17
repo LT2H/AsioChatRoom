a1be858de52d858ec885c2cc09ac9ff4bdfd505e
LT2H <letranhuuhung@gmail.com>
2025-06-17 21:42:15 +0700

    chore: remove unnecessary constexpr

--- apps/Client/ClientInfo.h
+++ apps/Client/ClientInfo.h
@@ -9,5 +9,5 @@ struct ClientInfo
 
     std::array<float, 3> color{ 1.0f, 1.0f, 1.0f };
 
-    constexpr std::string to_string() const { return std::string{ name.data() }; }
+    std::string to_string() const { return std::string{ name.data() }; }
 };
